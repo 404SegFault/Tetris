4f83b31d6cd81b72f4445412b61be22253a33dbb
404SegFault <daniel062588@gmail.com>
2022-02-25 23:39:21 +1100

    25/02 23:36 I ADDED ROTATION??
    Adding rotation

--- Tetris/src/main/java/tetris/Piece.java
+++ Tetris/src/main/java/tetris/Piece.java
@@ -1,33 +1,12 @@
 package tetris;
 
-<<<<<<< Updated upstream
-import java.util.ArrayList;
-import java.util.Random;
-=======
 import processing.core.PApplet;
 import processing.core.PImage;
 import processing.core.PFont;
 import processing.data.JSONObject;
->>>>>>> Stashed changes
 
 import java.util.*;
 
-<<<<<<< Updated upstream
-    Block topBlock;
-    Block bottomBlock;
-    String pieceDir;
-
-    public Piece(String colour1, String colour2){
-
-        String[] allColours = new String[]{"darkBlue", "green", "lightBlue", "orange", "purple", "red", "yellow"};
-        Random random = new Random();
-
-        String randomColour1 = allColours[(int)random.int(0,7).findFirst().getAsInt()];
-        String randomColour2 = allColours[(int)random.int(0,7).findFirst().getAsInt()];
-
-        String BlockOneName = colour1 + "Block";
-        String BlockTwoName = colour2 + "Block";
-=======
 
 public class Piece extends GameObject{
 
@@ -81,7 +60,6 @@ public class Piece extends GameObject{
                 newBottomX += App.GRIDSPACE;
                 newBottomY += App.GRIDSPACE;
                 System.out.printf("%d %d\n",newBottomX,newBottomY);
->>>>>>> Stashed changes
 
                 pieceDir = "down";
                 break;
@@ -91,90 +69,13 @@ public class Piece extends GameObject{
                 newBottomY -= App.GRIDSPACE;
                 System.out.printf("%d %d\n",newBottomX,newBottomY);
 
-<<<<<<< Updated upstream
-        this.topBlock = new Block(allSprites.get(BlockOneName), topX, topY);
-        this.bottomBlock = new Block(allSprites.get(BlockTwoName), bottomX, bottomY);
-
-        this.topBlock.setDown(bottomBlock);
-        this.pieceDir = "down";
-
-    }
-
-    public void pieceCCWRotation(){
-        //rotates along topblock
-        
-        int newBottomX = this.topBlock.getCoords()[0];
-        int newBottomY = this.topBlock.getCoords()[1];
-
-        switch(this.blockDir){
-            case "down":
-                newBottomX += 32;
-                newBottomY -= 32;
-                this.pieceDir = "right";
-
-            case "up":
-                newBottomX -= 32;
-                newBottomY += 32;
-                this.pieceDir = "left";
-
-            case "left":
-                newBottomX += 32;
-                newBottomY += 32;
-                this.pieceDir = "down";
-
-            case "right":
-                newBottomX -= 32;
-                newBottomY -= 32;
-                this.pieceDir = "up";
-        }
-
-        bottomBlock.setCoord(newBottomX, newBottomY);
-    }
-
-    public void pieceCWRotation(){
-        //rotates along topblock
-        
-        int newBottomX = this.topBlock.getCoords()[0];
-        int newBottomY = this.topBlock.getCoords()[1];
-
-        switch(this.blockDir){
-            case "down":
-                newBottomX -= 32;
-                newBottomY -= 32;
-                this.pieceDir = "left";
-
-            case "up":
-                newBottomX += 32;
-                newBottomY += 32;
-                this.pieceDir = "right";
-
-            case "left":
-                newBottomX += 32;
-                newBottomY -= 32;
-                this.pieceDir = "up";
-
-            case "right":
-                newBottomX -= 32;
-                newBottomY += 32;
-                this.pieceDir = "down";
-        }
-
-        bottomBlock.setCoord(newBottomX, newBottomY);
-    }
-
-    
-    public void pieceGeneration(){
-=======
                 pieceDir = "up";
                 break;
         }
->>>>>>> Stashed changes
 
         bottomBlock.setCoord(newBottomX, newBottomY);
     }
 
-<<<<<<< Updated upstream
-=======
     public void pieceCWRotation(){
         //rotates along topblock
         
@@ -224,6 +125,5 @@ public class Piece extends GameObject{
         
     // }
 
->>>>>>> Stashed changes
     
 }
