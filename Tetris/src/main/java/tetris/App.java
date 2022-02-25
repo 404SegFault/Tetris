f9223934de061e945cd9c1a115ce3803d2820578
404SegFault <daniel062588@gmail.com>
2022-02-25 23:53:29 +1100

    Updating 23:54

--- Tetris/src/main/java/tetris/App.java
+++ Tetris/src/main/java/tetris/App.java
@@ -15,18 +15,21 @@ public class App extends PApplet {
     public static final int FPS = 60;
 
 	private PFont font;
-
+	private HashMap<String, PImage> allSprites;
+	private ArrayList<GameObject> allObjects;
 	private JSONObject config;
 
-	private HashMap<String, PImage> allSprites = new HashMap<>();
-	private ArrayList<Block> allBlocks = new ArrayList<>(); //anything that is put in all blocks gets drawn
+	// private HashMap<String, PImage> allSprites = new HashMap<>();
+	private ArrayList<Block> allBlocks = new ArrayList<>();
 	private DropTimer dropTimer;
 	private int dropMilliseconds = 2000;
 
 	private String[] colours = new String[]{"DarkBlue","Green","LightBlue","Orange","Purple","Red","Yellow"};
 
-	
 	private Block moveableBlock;
+
+	private Piece piece;
+
 
 	/////////////////////////////CREATING THE APP OBJECT//////////////////////////
     public void settings() {
@@ -35,7 +38,8 @@ public class App extends PApplet {
 
 	/**Loads the files inside the resources into the application, where it can be used in the game**/
     public void setup() {
-
+		HashMap<String, PImage> allSprites = new HashMap<>();
+		this.allObjects = new ArrayList<GameObject>();
         frameRate(FPS);
 
 		allSprites.put("DarkBlue", loadImage("Tiles/tileDarkBlue.png"));
@@ -43,10 +47,24 @@ public class App extends PApplet {
 		allSprites.put("LightBlue", loadImage("Tiles/tileLightBlue.png"));
 		allSprites.put("Orange", loadImage("Tiles/tileOrange.png"));
 		allSprites.put("Purple", loadImage("Tiles/tilePurple.png"));
-		allSprites.put("Red", loadImage("Tiles/tileRed.png"));
+		// allSprites.put("Red", loadImage("Tiles/tileRed.png"));
 		allSprites.put("Yellow", loadImage("Tiles/tileYellow.png"));
 
+		allSprites.put("Red_Red", loadImage("pills/Red_Red.png"));
+		allSprites.put("Blue_Blue", loadImage("pills/Blue_Blue.png"));
+		allSprites.put("Blue_Green", loadImage("pills/Blue_Green.png"));
+		allSprites.put("Blue", loadImage("pills/Blue.png"));
+		allSprites.put("Green_Green", loadImage("pills/Green_Green.png"));
+		allSprites.put("Green_Red", loadImage("pills/Green_Red.png"));
+
+		String[] pillFiles = new String[]{"Red_Red", "Blue_Blue", "Blue_Green", "Blue", "Green_Green", "Green_Red", "Green", "Red_Blue", "Red_Red", "Red"};
+
+		// for (int i = 0; i <  pillFiles.size(); i++){
+		// 	allSprites.put(pillFiles, loadImage("pills/" + pillFiles + ".png"));
+		// }
+
 		config = loadJSONObject("config.json");
+		// this.timeRemaining = config.getJSONArray("levels").getJSONObject(0).getInt("time"); maybe a level system
 		
 		Timer timer = new Timer();
 
@@ -56,6 +74,9 @@ public class App extends PApplet {
 		timer.schedule(dropTimer, 0, dropMilliseconds);
 
 		Block block = new Block(allSprites.get("DarkBlue"), 320, 0, "DarkBlue");
+
+		this.piece = new Piece(allSprites, allSprites.get("Red_Red"),320, 320, "Red", "Red");
+
 		this.allBlocks.add(block);
 
 		this.font = createFont("PressStart2P-Regular.ttf", 20);
@@ -64,38 +85,29 @@ public class App extends PApplet {
     }
 	
 	/////////////////////////METHODS USED WHEN RUNNING THE GAME////////////////////
+	public void drawUI(){
+		background(112, 123, 138);
+		for(GameObject gameObject : this.allObjects){ gameObject.draw(this); }
+
+	}
+
 	/** Decriments the timer every second, moves enemies**/
 	public void tick(){
 		this.frameCount++;
 
 		// if a second passes
 		if (frameCount % 60 == 0){
-
+			// this.timeRemaining -= 1;
 		}
-
-		// FIXME THIS IS SO FUCKING BAD, IT SHOULDNT HAVE TO CHECK EVERY TICK BUT IDK WHERE ELSE
-		// goes through all the blocks
-		for (int i = 0; i < this.allBlocks.size(); i++){
-
-			// goes through all the blocks and checks whether it is set or not if it is not set then you can still move it
-			if (this.allBlocks.get(i).getSet() == false){
-				this.moveableBlock = this.allBlocks.get(i);
-			}
-		}
-
 	}
 
 	/** Goes through all the objects and draws them **/
     public void draw() {
 		this.tick();
-		this.background(112, 123, 138);
+		this.drawUI();
 
-		for (int i = 0; i < this.allBlocks.size(); i++){
-			allBlocks.get(i).draw(this);
-		}
-
+		this.piece.draw(this);
 		//---------------------DRAWING THE OBJECTS------------------------
-		
     }
 
 	/** detects when a key is pressed and calls the player method for movement **/
@@ -110,11 +122,21 @@ public class App extends PApplet {
 			case PApplet.DOWN:
 				moveableBlock.moveDown();
 				break;
+
+			case 88:
+				piece.pieceCWRotation();
+				break;
+			case 90:
+				piece.pieceCCWRotation();
+				break;
+
 		}
 	}
 	
 	/** changes can move to true so that the player can move again**/
-	public void keyReleased(){}
+	public void keyReleased(){ 
+		;
+	}
 
 	/////////////////////////////////GAME SCREENS//////////////////////////////////////
 	public void displayGameOver(){
@@ -136,14 +158,3 @@ public class App extends PApplet {
 	public ArrayList<Block> getAllBlocks(){return this.allBlocks;}
 }
 
-// generate random colour
-// Random rand = new Random();
-
-// String colour = this.colours[rand.nextInt(7)];
-
-// Block block = new Block(allSprites.get(colour), 320, 0, colour);
-// this.allBlocks.add(block);
-
-// for (int i = 0; i < this.allBlocks.size(); i++){
-// 		this.allBlocks.get(i).moveDown();
-// 	}
